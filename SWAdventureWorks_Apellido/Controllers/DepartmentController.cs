75140a17564a1cd6dec7ad085a276bb96487e635
Hlorenzoo <hernanlorenzo97@hotmail.com>
2023-01-25 15:27:09 -0300

    Agregar archivos de proyecto.

---
+++ SWAdventureWorks_Apellido/Controllers/DepartmentController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SWAdventureWorks_Apellido.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SWAdventureWorks_Apellido.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly AdventureWorks2019Context context;
+
+        public DepartmentController(AdventureWorks2019Context context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Department>> Get()
+        {
+            return context.Department.ToList();
+        }
+
+        [HttpGet("{ID}")]
+        public ActionResult<Department> GetById(short ID)
+        {
+            Department department = (from e in context.Department
+                           where e.DepartmentId == ID
+                           select e).SingleOrDefault();
+            return department;
+        }
+
+        [HttpGet("name/{name}")]
+        public ActionResult<Department> GetByName(string name)
+        {
+
+            Department department = (from e in context.Department
+                           where e.Name == name
+                           select e).SingleOrDefault();
+            return department;
+        }
+
+        [HttpPost]
+        public ActionResult<Department> Post(Department department)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            context.Department.Add(department);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        }
+    }
